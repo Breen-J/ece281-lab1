30a08ba182f97b597239aed586dc9761c0efa165
Breen-J <141793138+Breen-J@users.noreply.github.com>
2024-02-13 11:36:25 -0700

    Lab 1... Re-Commit Attempt via GitBash

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -11,7 +11,7 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thirtyOneDayMonth.vhd
---| AUTHOR(S)     : Capt Dan Johnson, ***Your Name Here***
+--| AUTHOR(S)     : Capt Dan Johnson, ***Breen, James***
 --| CREATED       : 12/12/2019 Last Modified 06/24/2020
 --| DESCRIPTION   :  This file implements the thirtyOneDaMonth lab.  Using a 4
 --| switch input, the circuit will light up whenever the 4 switches represent a
@@ -55,25 +55,49 @@ library ieee;
 -- entity name should match filename, this has been filled out for you  
 entity thirtyOneDayMonth is 
   port(
-	i_A : in std_logic; -- one of four inputs
-	
-	
-	
-						-- output
+	i_A    : in std_logic; -- one of four inputs
+	i_B    : in std_logic;
+	i_C    : in std_logic;
+	i_D    : in std_logic;
+	o_Y    : out std_logic				-- output
   );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
 	
+	signal w_sel : std_logic_vector (3 downto 0); -- MUX Sel
+	
 	--signals internal to the architecture are declared and initialized such as w_sel
   
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
-	w_sel(0) <= i_C;	-- one
+	w_sel(0) <= i_D;	-- one
+	w_sel(1) <= i_C;
+	w_sel(2) <= i_B;
+	w_sel(3) <= i_A;
 	--finish assigning signals
 	
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
-	---------------------------------------------------------------	
+	
+    o_Y <= '0' when (w_sel = "0000") else -- 0
+	       '1' when (w_sel = "0001") else -- JAN
+	       '0' when (w_sel = "0010") else -- FEB
+	       '1' when (w_sel = "0011") else -- MARCH
+	       '0' when (w_sel = "0100") else -- APRIL
+           '1' when (w_sel = "0101") else -- MAY
+           '0' when (w_sel = "0110") else -- JUNE
+           '1' when (w_sel = "0111") else -- JULY
+           '1' when (w_sel = "1000") else -- AUG
+           '0' when (w_sel = "1001") else -- SEP
+           '1' when (w_sel = "1010") else -- OCT
+           '0' when (w_sel = "1011") else -- NOV
+           '1' when (w_sel = "1100") else -- DEC
+           '0' when (w_sel = "1101") else -- 13
+           '0' when (w_sel = "1110") else -- 14    
+           '0' when (w_sel = "1111") else -- 14       
+	'0';
+    	
+	----------------------`-----------------------------------------	
 end thirtyOneDayMonth_arch;
